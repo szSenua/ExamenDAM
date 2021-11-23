97c9640b9c20c023c2e1bdbafa5d5393bbee99e9
Cristina Hidalgo Cobo <87225960+Xhexdoit@users.noreply.github.com>
2021-11-23 17:02:31 +0100

    Update Maintitularesycuentas.java

--- Maintitularesycuentas.java
+++ Maintitularesycuentas.java
@@ -7,7 +7,7 @@ public class Maintitularesycuentas {
     public static void main(String[] args) {
         //Crear titulares
         
-        Titular t1=new Titular("Cristina", "Hidalgo", 31);
+        Titular t1=new Titular("Cristina", "Monteiro", 31);
         Titular t2=new Titular("Leandro", "Gao", 35);
         Titular t3=new Titular("Bruce", "Wayne", 40);
         
